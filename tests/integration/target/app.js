e9f38842a439b0339b0c5b999da1aae6a8f67da1
Smith, Greg W <Greg.W.Smith@nordstrom.com>
2018-07-12 17:43:02 -0700

    WIP: Results via CWL.

--- tests/integration/target/app.js
+++ tests/integration/target/app.js
@@ -1,4 +1,6 @@
+const AWS = require('aws-sdk')
 const express = require('express')
+const uuid = require('uuid').v4
 
 const app = express()
 
@@ -28,6 +30,50 @@ const stats = () => {
   }
 }
 
+const statsCW = (bucketName) => {
+  let runId = uuid()
+
+  return {
+    reset: () => { runId = uuid() },
+    requests: async () => {
+      const requests = {}
+      const params = {
+        Bucket: bucketName,
+        MaxKeys: 1000,
+        Prefix: runId,
+      }
+      const data = await s3.listObjectsV2(params).promise()
+      const keysToQuery = data.Contents.map(obj => obj.Key)
+
+      keysToQuery.forEach(async (key) => {
+        const objParams = {
+          Bucket: bucketName,
+          Key: key,
+        }
+        const requestsObj = await s3.getObject(objParams).promise()
+        const requestsPaths = JSON.parse(requestsObj.Body)
+
+        requests[key] = requestsPaths
+      })
+
+      return requests
+    },
+    totals: async () => {
+      const totals = {}
+      const requests = await this.requests()
+      Object.keys(requests)
+        .forEach((time) => {
+          requests[time].forEach((sample) => {
+            totals[sample] = totals[sample] ?
+              totals[sample] + 1 : 1
+          })
+        })
+      return totals
+    },
+    count: req => console.log(`REQUEST|${req.path}`),
+  }
+}
+
 const theStats = stats()
 
 app.get('/reset', (req, res) => {
@@ -36,22 +82,24 @@ app.get('/reset', (req, res) => {
   res.send(`RESET @ ${Date.now()}`)
 })
 
-app.get('/requests', (req, res) => {
+app.get('/requests', async (req, res) => {
   console.log('reporting requests')
+  const requests = await theStats.requests()
   res
     .type('application/json')
-    .send(JSON.stringify(theStats.requests()))
+    .send(JSON.stringify(requests))
 })
 
-app.get('/totals', (req, res) => {
+app.get('/totals', async (req, res) => {
   console.log('reporting totals')
+  const totals = await theStats.totals()
   res
     .type('application/json')
-    .send(JSON.stringify(theStats.totals()))
+    .send(JSON.stringify(totals))
 })
 
 app.all('/*', (req, res) => {
-  console.log(req.path)
+  console.log(`REQUEST|${req.path}`)
   theStats.count(req)
   res.send(`OK - ${req.path}`)
 })
