0834f457833784f0b39f659acd868c9ac116ca7f
Parsi, Sayna <sayna.parsi@nordstrom.com>
2018-09-11 10:13:17 -0700

    changing maxChunkRequestsPerSecond to 1, fixing load script

--- tests/integration/load/load.js
+++ tests/integration/load/load.js
@@ -7,28 +7,27 @@ const scriptPath = __dirname + '/load_script.yml'
 
 const baseScript = idioms.parseInput(scriptPath)
 const basic = idioms.phaseUpdate(baseScript, {})
-const horizontal_test = idioms.phaseUpdate(baseScript, {"duration": 20, "arrivalRate": 5})
-const vertical_test = idioms.phaseUpdate(baseScript, {"duration": 2, "arrivalRate": 50})
-const horizontal_and_vertical = idioms.phaseUpdate(baseScript, {"duration": 20, "arrivalRate": 50})
+const horizontal_test = idioms.phaseUpdate(baseScript, {"duration": 15, "arrivalRate": 1})
+const vertical_test = idioms.phaseUpdate(baseScript, {"duration": 1, "arrivalRate": 3})
+const horizontal_and_vertical = idioms.phaseUpdate(baseScript, {"duration": 15, "arrivalRate": 3})
 
 module.exports = () =>
   idioms.runIn(__dirname, () =>
     ([
-      { data: basic, scenariosCreated: 10 },
-      { data: horizontal_test, scenariosCreated: 100 },
-      { data: vertical_test, scenariosCreated: 100 },
-      { data: horizontal_and_vertical, scenariosCreated: 1000 }
+      { data: basic, scenarioCount: 1 },
+      { data: horizontal_test, scenarioCount: 15 },
+      { data: vertical_test, scenarioCount: 3 },
+      { data: horizontal_and_vertical, scenarioCount: 45 }
     ])
     .reduce(
-      (promise, {data, scenariosCreated}) => {
-        return promise
+      (promise, {data, scenarioCount}) => promise
         .then(idioms.functionDoesNotExist())
         .then(() => BbPromise.resolve()
           .then(idioms.deploy())
           .then(idioms.functionExists())
-          .then(idioms.invoke({ "data" : JSON.stringify(basic)}))
-          .then(idioms.expect({ aggregate: { scenariosCreated: 10 } }))
+          .then(idioms.invoke({ "data" : JSON.stringify(data)}))
+          .then(idioms.expect({ aggregate: { scenariosCreated: scenarioCount } }))
           .then(idioms.remove(), idioms.remove())
-          .then(idioms.functionDoesNotExist()))},
+          .then(idioms.functionDoesNotExist())),
       BbPromise.resolve())
 )
